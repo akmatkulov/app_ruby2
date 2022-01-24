7b10aecfd85cb6a181c58556b98c2a57f2849d81
akmatkulov <akmatkulovs991@gmail.com>
2022-01-25 02:25:56 +0600

    Add def greeting

--- hello.rb
+++ hello.rb
@@ -1,2 +1,13 @@
-puts 'Hello World'
- puts 'Hello JetRuby Academy'
+def greeting(firstname, lastname, age)
+  puts "Привет.#{firstname} #{lastname}. Тебе меньше 18 лет, но начать учиться программировать никогда не рано" if age < 18
+  puts "Привет.#{firstname} #{lastname}. Самое время занятся делом" if age > 18
+end
+
+print "Введите ваше имя:"
+firstname = gets.chomp
+print "Введите вашу фамилию:"
+lastname = gets.chomp
+print "Введите ваш возраст:"
+age = gets.to_i
+
+greeting(firstname, lastname, age)

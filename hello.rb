ebd08bea914d6903336784a50e37a2f21e66dd2a
akmatkulov <akmatkulovs991@gmail.com>
2022-01-26 00:54:21 +0600

    Add def greeting

--- hello.rb
+++ hello.rb
@@ -1,6 +1,9 @@
 def greeting(firstname, lastname, age)
-  puts "Привет.#{firstname} #{lastname}. Тебе меньше 18 лет, но начать учиться программировать никогда не рано" if age < 18
-  puts "Привет.#{firstname} #{lastname}. Самое время занятся делом" if age > 18
+  if age < 18
+    "Привет. #{firstname} #{lastname}. Тебе меньше 18 лет, но начать учиться программировать никогда не рано"
+  else
+    "Привет. #{firstname} #{lastname}. Самое время занятся делом"
+  end
 end
 
 print "Введите ваше имя:"
